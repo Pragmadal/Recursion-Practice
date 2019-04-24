b63ea0f72503fb646ccbf866b28ad6b0fb6f3de6
Pragmatic_Sandal <47468616+Pragmadal@users.noreply.github.com>
2019-04-24 11:52:55 -0500

    Update sak66330_a16.java

--- sak66330_a16.java
+++ sak66330_a16.java
@@ -8,12 +8,39 @@ public class sak66330_a16 {
         Scanner input = new Scanner(System.in);
         int n = input.nextInt(), k = input.nextInt();
         array = new int[n+1][k+1];
-        System.out.println(coefficients(n, k) + " " + callCount1);
-        System.out.println(coefficients2(n, k) + " " + callCount2);
+        System.out.println(coefficients1(n, k));
+        System.out.println(coefficients2(n, k) + " " + callCount1);
+        System.out.println(coefficients3(n, k) + " " + callCount2);
         printArray(array);
     }
 
-    public static int coefficients2(int n, int k) {
+    public static int coefficients1(int n, int k) {
+		int numer = 0, denom1 = 0, denom2 = 0;
+		for (int i = 0; i < n; i++) {
+			if (i == 0)
+				numer = n; denom1 = k; denom2 = n - k;
+			if (i > 0) numer *= n - i;
+		}	
+		for (int i = 0; i < k; i++) {
+			if (i == 0)
+				denom1 = k;
+			if (i > 0) denom1 *= k - i;
+		}	
+		for (int i = 0; i < n - k; i++) {
+			if (i == 0)
+				denom2 = n - k;
+			if (i > 0) denom2 *= n - k - i;
+		}	
+		return numer / (denom1 * denom2);
+	}
+    
+     public static int coefficients2(int n, int k) {
+        callCount1++;
+        if (n == k || k == 0) return 1;
+        else return coefficients2(n - 1, k) + coefficients2(n - 1, k - 1);
+    }
+    
+    public static int coefficients3(int n, int k) {
         callCount2++;
         if (array[n][k] != 0) return array[n][k];
         else if (n == k || k == 0) {
@@ -21,15 +48,9 @@ public class sak66330_a16 {
             return 1;
         }
         else {
-            array[n][k] = coefficients2(n - 1, k) + coefficients2(n - 1, k - 1);
+            array[n][k] = coefficients3(n - 1, k) + coefficients3(n - 1, k - 1);
             return array[n][k];
         }
-    }
-
-    public static int coefficients(int n, int k) {
-        callCount1++;
-        if (n == k || k == 0) return 1;
-        else return coefficients(n - 1, k) + coefficients(n - 1, k - 1);
     }
 
     public static void printArray(int[][] array) {
